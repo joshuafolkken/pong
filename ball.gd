8aa072eefe1722a8532d4bddbb53a290645f19ab
sinYa Iwasaki <iwasaki@sinproject.net>
2024-04-12 00:17:59 +0900

    Refrect with paddle #8

--- ball.gd
+++ ball.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const INIT_SPEED = 200
+const INIT_SPEED = 300
 const ACCELERATION = 10
 
 var speed := INIT_SPEED
@@ -20,24 +20,25 @@ func _physics_process(delta: float) -> void:
 
 		var collider: Node2D = collision.get_collider()
 
-		if collider.is_in_group("paddle"):
-			print("paddle")
-			velocity = velocity.bounce(collision.get_normal())
+		if collider.is_in_group("paddles"):
+			# print("paddle")
+			# velocity = velocity.bounce(collision.get_normal())
 			speed += ACCELERATION
 
-			# var paddle_position_y := collider.global_position.y
-			# var distance := global_position.y - paddle_position_y
-			# var max_distance: int = collider.get_node("CollisionShape2D").shape.extents.y
-			# var normalizerd_distance := distance / max_distance
-			# var angle := normalizerd_distance * (PI / 4)
+			var paddle_position_y := collider.position.y
+			var hit_pos := position.y - paddle_position_y
+			var max_distance: int = collider.get_node("CollisionShape2D").shape.extents.y
+			var normalizerd_distance := hit_pos / max_distance
+			var angle := normalizerd_distance * (PI / 4)
+			var direction := Vector2.RIGHT.rotated(angle)
 
-			# # velocity.x *= 1
-			# # velocity.y = normalizerd_distance
+			if collider.is_in_group("left_paddle"):
+				direction.x = abs(direction.x)
+			else:
+				direction.x = -abs(direction.x)
 
-			# speed += ACCELERATION
-			# # velocity = velocity.normalized() * speed * delta
+			velocity = velocity.length() * direction
 
-			# velocity = velocity.bounce(collision.get_normal()).rotated(angle)
 		elif collider.is_in_group("wall"):
 			print("wall")
 			velocity = velocity.bounce(collision.get_normal())
