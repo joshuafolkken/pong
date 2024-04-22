0331284115340c4205cb310c532793f056a7da23
sinYa Iwasaki <iwasaki@sinproject.net>
2024-04-22 18:51:00 +0900

    Interlace Artifacts #98

--- web/index.service.worker.js
+++ web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1713762537|16288462772';
+const CACHE_VERSION = '1713779445|5045471666';
 /** @type {string} */
 const CACHE_PREFIX = 'PONG-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
