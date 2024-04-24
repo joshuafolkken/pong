2b14175483139bb867dbe4cab61d2bda57834d49
sinYa Iwasaki <iwasaki@sinproject.net>
2024-04-24 18:37:41 +0900

    Retro toggle button #123

---
+++ scripts/texture_button.gd
@@ -0,0 +1,21 @@
+extends TextureButton
+
+@export var on_color := Color(1, 1, 1, 1)
+@export var off_color: Color = Color(1, 1, 1, 0.2)
+
+func _ready() -> void:
+	toggle_mode = true
+
+	change_color()
+	toggled.connect(_on_toggled)
+
+
+func change_color() -> void:
+	if button_pressed:
+		modulate = on_color
+	else:
+		modulate = off_color
+
+
+func _on_toggled(_toggled_on: bool) -> void:
+	change_color()
